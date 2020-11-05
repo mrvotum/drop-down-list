78746e37e412326069c518ce4442516121a5ac1f
Vladimir <mr.qrsant@gmail.com>
2020-11-05 15:49:28 +0300

    Правки по linter'у

--- src/js/widget.js
+++ src/js/widget.js
@@ -1,62 +1,62 @@
 export default class Widget {
   constructor() {
-    this.listBtn = document.querySelector("[data-id=list__btn]");
-    this.dropDownList__main = document.querySelector("[data-id=drop-down-list__main]");
-    this.dropDownList__holder = document.querySelector("[data-id=drop-down-list__holder]");
-    this.listTitle = document.querySelector("[data-id=list__title]");
+    this.listBtn = document.querySelector('[data-id=list__btn]');
+    this.dropDownList__main = document.querySelector('[data-id=drop-down-list__main]');
+    this.dropDownList__holder = document.querySelector('[data-id=drop-down-list__holder]');
+    this.listTitle = document.querySelector('[data-id=list__title]');
   }
 
   create() {
     this.addListener();
   }
 
+  // eslint-disable-next-line class-methods-use-this
   listSeparator(gettetList) {
-    const elementsArr = gettetList.split("|");
-    let listArr = "";
-    
+    const elementsArr = gettetList.split('|');
+    let listArr = '';
+
     for (let i = 0; i < elementsArr.length; i += 1) {
       listArr += `<li class="drop-down-list__el">
             <input class="checkbox" type="checkbox" name="vacancy" value="vacancy_0${i}" data-id="vacancy_0${i}" data-vacancy="${elementsArr[i]}" id="vacancy_0${i}">
             <label class="checklist__label" for="vacancy_0${i}">${elementsArr[i]}</label>
           </li>`;
     }
-  
+
     return listArr;
   }
 
   createDropList() {
-    const ulEl = document.createElement("ul");
-  
-    ulEl.classList = "drop-down-list drop-down-list--hidden";
-    ulEl.setAttribute("data-id", "drop-down-list");
-  
+    const ulEl = document.createElement('ul');
+
+    ulEl.classList = 'drop-down-list drop-down-list--hidden';
+    ulEl.setAttribute('data-id', 'drop-down-list');
+
     ulEl.innerHTML = this.listSeparator(this.dropDownList__holder.dataset.list);
-  
+
     this.dropDownList__holder.appendChild(ulEl);
     setTimeout(() => {
-      ulEl.classList.remove("drop-down-list--hidden");
+      ulEl.classList.remove('drop-down-list--hidden');
     }, 50);
   }
 
   addListener() {
-    this.dropDownList__main.addEventListener("click", () => {
+    this.dropDownList__main.addEventListener('click', () => {
       if (this.listBtn.classList.length === 2) {
-        this.listBtn.classList.remove("list__btn--active");
-        document.querySelector("[data-id=drop-down-list]").classList.add("drop-down-list--hidden");
+        this.listBtn.classList.remove('list__btn--active');
+        document.querySelector('[data-id=drop-down-list]').classList.add('drop-down-list--hidden');
 
         // Удалим список
         setTimeout(() => {
           this.addCheckboxListener();
 
-          document.querySelector("[data-id=drop-down-list]").remove();
+          document.querySelector('[data-id=drop-down-list]').remove();
         }, 500);
-       } else {
-        this.listBtn.classList.add("list__btn--active");
+      } else {
+        this.listBtn.classList.add('list__btn--active');
         this.createDropList();
 
         // Если уже есть выбранные вакансии, то тогда вернём галочки на них
         if (this.dropDownList__holder.getAttribute('data-listchecked')) {
-          console.log('Во, есть отмеченные');
           this.createCheckingVacancy();
         }
       }
@@ -65,31 +65,26 @@ export default class Widget {
 
   addCheckboxListener() {
     const checkboxes = document.querySelectorAll('input[name="vacancy"]:checked');
-    
+
     if (checkboxes.length !== 0) {
       const vacancyArr = [];
       this.vacancyArrContent = [];
       this.vacancyArrId = [];
-      console.log('начать заново, должен быть пустой vacancyArrId');
-      console.log(this.vacancyArrContent);
-  
+
       checkboxes.forEach((element) => {
         vacancyArr.push(this.dropDownList__holder.querySelector(`[data-id=${element.value}]`));
         this.vacancyArrContent.push(`${element.getAttribute('data-vacancy')},`);
         this.vacancyArrId.push(element.getAttribute('data-id'));
       });
-  
-      // тут можно посмотреть надписи вакансий выбранных
-      console.log(vacancyArr[0].getAttribute('data-vacancy'));
-  
+
       // Чтобы не потерять галочки
-      this.dropDownList__holder.setAttribute("data-listchecked", `${this.vacancyArrId}`);
+      this.dropDownList__holder.setAttribute('data-listchecked', `${this.vacancyArrId}`);
 
       this.createListTitle();
     } else {
       // Значит, что не выбрали вакансий никаких
       // Чтобы при повторном снятии очистить все галочки
-      this.dropDownList__holder.removeAttribute("data-listchecked");
+      this.dropDownList__holder.removeAttribute('data-listchecked');
       this.listTitle.textContent = 'Выберите интересующие вакансии';
     }
   }
@@ -119,7 +114,7 @@ export default class Widget {
           let el = element[i];
 
           if (el === ',' && this.vacancyArrContent.indexOf(element) !== this.vacancyArrContent.length - 1) {
-            el = `${el} `
+            el = `${el} `;
           } else if (el === ',') {
             el = '';
           }
@@ -128,11 +123,12 @@ export default class Widget {
         }
       });
 
-      // Если в строку не помещается весь текст, добавить многоточие (52 - макс кол-во символов с строке)
+      // Если в строку не помещается весь текст,
+      // добавить многоточие (52 - макс кол-во символов с строке)
       if (titleVacancyArr.length >= 52) {
         titleVacancyArr = `${titleVacancyArr}...`;
       }
-      
+
       this.listTitle.textContent = titleVacancyArr;
     }
   }
